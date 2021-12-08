36b4d261b93395d560979c79fc6dd5cd55e5469d
opilane <opilane@HP-CZC6297BBL>
2021-12-08 12:36:09 +0200

    fed

--- _7.12_slovari.py
+++ _7.12_slovari.py
@@ -3,17 +3,18 @@ from keyboard import *
 Countries={}     #Создаем словарь с помощью литерала
 Countries=dict() #Создал пустой словарь под названием Countries
 Capitals={}     #Создаем словарь с помощью литерала
-Capitals=dict()     #Создал пустой словарь под названием Capitals
-Countries=loe_failist_listisse("riigid.txt")
-Capitals=loe_failist_listisse("pealinn.txt")
+Capitals=dict() #Создал пустой словарь под названием Capitals
+Countries=loe_failist_listisse("riigid.txt") #читаем файл в список из .txt 
+Capitals=loe_failist_listisse("pealinn.txt") #читаем файл в список из .txt 
+country=input("Siseta riik => ")
 
 for country in Countries:
-    if country in Capitals:
-        print('Столица страны ' + country + ': ' + Capitals[country])
+    if country in Countries:
+        print('Столица страны ' + Capitals[country])
     else:
-        print('В базе нет страны c названием ' + country) #Проверим, есть ли она в словаре Capitals
+        print('В базе нет страны c названием ' + country)
 
 
 
-riigid=loe_failist_listisse("riigid.txt")
-pealinn=loe_failist_listisse("pealinn.txt")
+
+       
