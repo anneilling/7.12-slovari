e135c397b5d2257d6c9e195701d7af5ac8f33a03
opilane <opilane@HP-CZC6297BBL>
2021-12-09 09:56:39 +0200

    .

--- _7.12_slovari.py
+++ _7.12_slovari.py
@@ -1,20 +1,20 @@
-from module1 import *
-from keyboard import *
-Countries={}     #Создаем словарь с помощью литерала
-Countries=dict() #Создал пустой словарь под названием Countries
-Capitals={}     #Создаем словарь с помощью литерала
-Capitals=dict() #Создал пустой словарь под названием Capitals
-Countries=loe_failist_listisse("riigid.txt") #читаем файл в список из .txt 
-Capitals=loe_failist_listisse("pealinn.txt") #читаем файл в список из .txt 
-country=input("Siseta riik => ")
 
-for country in Countries:
-    if country in Countries:
-        print('Столица страны ' + Capitals[country])
-    else:
-        print('В базе нет страны c названием ' + country)
-
-
+sonastik={}
+riigid=[]
+linnad=[]
+file=open("Text.txt", "r")
+for line in file:
+    k, v=line.strip().split(" - ")
+    sonastik[k.strip()] = v.strip()
+    riigid.append(k)
+    linnad.append(sonastik[k.strip()])
+file.close()
+print(sonastik)
+print("Riigid: ")
+print(riigid)
+print("Pealinnad: ")
+print(linnad)
+a=input()
 
 
        
