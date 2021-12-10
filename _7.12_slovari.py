0ba2d395467f6cb44c19c367530153e9a360a3e9
opilane <opilane@HP-CZC6297BBL>
2021-12-10 12:56:07 +0200

    ДОБАВИЛА: - отображение столицы, если вводиться название государства и наоборот. - если искомое слово отсутствует в словаре, дайте пользователю возможность добавить его в словарь. -проверку -возможность воспроизводить аудио файл -всевозможные комментарии и даже commit <3

--- _7.12_slovari.py
+++ _7.12_slovari.py
@@ -1,20 +1,47 @@
+from gtts import gTTS
+import os
 
-sonastik={}
-riigid=[]
-linnad=[]
-file=open("Text.txt", "r")
-for line in file:
-    k, v=line.strip().split(" - ")
+sonastik={} #sõnastiku loomine
+riigid=linnad=[]
+file=open("Text.txt", "r") #tekstifaili avamine
+for line in file: #tsükkli loomine
+    k, v=line.strip().split(" - ") #
     sonastik[k.strip()] = v.strip()
-    riigid.append(k)
-    linnad.append(sonastik[k.strip()])
-file.close()
-print(sonastik)
-print("Riigid: ")
-print(riigid)
-print("Pealinnad: ")
-print(linnad)
-a=input()
+    riigid.append(k.strip()) #sisestame iga valitud üksuse lõppu meetodi parameetris määratud sisu ja tagastame riidu koopia
+    linnad.append(v) 
+file.close() #paneme faili kinni
+
+
+while True: #mugavuse huvids kasutame tsükkli
+    print("Euroopa riigid")
+    print("1 - Tunnustame riiki või pealinna\n2 - Lisame uut riiki või pealinna\n3 - Kontrolli enda") #kasutaja teeb valiku
+    v=int(input())
+    if v==1:
+        b=int(input("Pealinna teadmiseks - 1\nRiiki pealinnast teadmiseks - 2\n"))
+        if b==1:
+            riik=input("Sisestage riiki nime: ")
+            print("Selle riiki pealinn on ",sonastik[riik])
+        elif b==2:
+            linn=input("Sisestage pealinnu nime ")
+            print("Selle pealinnu riik on " , riik)
+        else:
+            print("1 / 2")
+    elif v==2:
+        a=input("Sisestage riik või pealinn mida soovite liisada, kasutage - sümboli ")
+        #file=open("Text.txt" , "x") #avab kirjutamiseks, kui faili pole olemas 
+        file=open("Text.txt" , "a") #liisab infot faili lõpuks
+    elif v==3:
+        v=input("Venemaa pealinn on... ")
+        if v=="Moskva":
+            print("Õige.")
+        else:
+           print("Vale!")
+    else:
+        print("Vale valik.") #kui kasutaja valis vale numbrit siis palume uuesti valida
+
+
+s=gTTS(text=linnad[8],lang="et",slow=False).save("heli.mp3") #määrame helifaili väärtus
+os.system("heli.mp3") #hellifaili esitamine
 
 
        
